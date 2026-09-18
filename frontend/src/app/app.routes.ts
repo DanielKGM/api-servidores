c71684cadbd63cfb8b64e1981fc01584f8d83c41
DanielKGM <atbreezy4@gmail.com>
2026-09-18 12:38:40 -0300

    feat: configuração de rotas entre as páginas da aplicação

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'secretarias',
+  },
+  {
+    path: 'secretarias',
+    loadComponent: () =>
+      import('./features/secretarias/pages/secretaria-list/secretaria-list').then(
+        (component) => component.SecretariaListComponent,
+      ),
+  },
+  {
+    path: 'secretarias/novo',
+    loadComponent: () =>
+      import('./features/secretarias/pages/secretaria-form/secretaria-form').then(
+        (component) => component.SecretariaFormComponent,
+      ),
+  },
+  {
+    path: 'secretarias/:id/editar',
+    loadComponent: () =>
+      import('./features/secretarias/pages/secretaria-form/secretaria-form').then(
+        (component) => component.SecretariaFormComponent,
+      ),
+  },
+  {
+    path: 'servidores',
+    loadComponent: () =>
+      import('./features/servidores/pages/servidor-list/servidor-list').then(
+        (component) => component.ServidorListComponent,
+      ),
+  },
+  {
+    path: 'servidores/novo',
+    loadComponent: () =>
+      import('./features/servidores/pages/servidor-form/servidor-form').then(
+        (component) => component.ServidorFormComponent,
+      ),
+  },
+  {
+    path: 'servidores/:id/editar',
+    loadComponent: () =>
+      import('./features/servidores/pages/servidor-form/servidor-form').then(
+        (component) => component.ServidorFormComponent,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'secretarias',
+  },
+];
